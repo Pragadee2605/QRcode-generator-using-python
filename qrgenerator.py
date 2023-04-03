5df8fbfff52af6cd242019043c847c83374de4a7
Pragadeeswaran.P <129708832+Pragadee2605@users.noreply.github.com>
2023-04-03 08:44:12 +0530

    Update qrgenerator.py

--- qrgenerator.py
+++ qrgenerator.py
@@ -5,7 +5,7 @@ qr=qrcode.QRCode(
     box_size=10,
     border=5
 )
-data="https://www.linkedin.com/in/pragadeeswaran-p-0669a9234/"
+data="https://www.linkedin.com/in/pragadeeswaran-p-0669a9234/"#you can give any link  address
 qr.add_data(data)
 qr.make(fit=True)
 img=qr.make_image(fill="black",black_colour="white")
